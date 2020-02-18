4d74049decc3a688829005c1bf95f5be13059df8
Archit Arora <archita@andrew.cmu.edu>
2020-02-18 14:46:48 -0500

    fixing typo on articles model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,8 @@ class Article < ApplicationRecord
 
 	belongs_to :category
 
-	validates_prescence_of :title
-	validates_prescence_of :content
+	validates_presence_of :title
+	validates_presence_of :content
 
 	scope :alphabetical, -> { order('title') }
 	scope :active, 			 -> { where('active = ?', true) }
